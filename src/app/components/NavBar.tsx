f366d5941044171c99239031b5700c617e8ca1fe
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2025-03-06 13:55:47 -0500

    NAv

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function NavBar() {
   return (
     <div className="flex items-center justify-items-start p-6 pb-20 gap-16 sm:p-10 tracking-tighter">
-      <h1 className="text-3xl">echo</h1>
+      <h1 className="text-3xl flex items-center gap-2">
+        echo
+        <Image src="/protocolLogo.svg" alt="Protocol Logo" width={50} height={50} />
+
+      </h1>
       <div className="flex items-center justify-between w-full">
 <div></div>
         <div className="flex items-center justify-between gap-4">
