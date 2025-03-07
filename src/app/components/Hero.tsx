17fe2bf5f8d1de6b956994ae844c36f9a5f39a86
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2025-03-07 02:13:03 -0500

    UI

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 import { ContainerScroll } from "./container-scroll-animation";
 import Image from "next/image";
+import Link from "next/link";
+import { HoverBorderGradient } from "../components/hover-border-gradient";
 
 function Hero() {
   return (
-    <section className="h-screen bg-black rounded-md !overflow-visible relative flex flex-col items-center justify-center antialiased px-4">
+    <section className="h-screen bg-black !overflow-visible relative flex flex-col items-center justify-center antialiased">
       <ContainerScroll
         titleComponent={
-          <div className="flex items-center flex-col">
-            <h1 className="text-5xl mt-40  md:text-5xl tracking-tighter pb-8 text-center max-w-4xl mx-auto">
-              personal conversations, personal results, personal growth
+          <div className="flex items-center flex-col mt-30">
+            <div className="flex items-center justify-center pb-4 hover:scale-105 transition-all duration-300 cursor-pointer">
+              <Link href="/sign-up">
+                <HoverBorderGradient>
+                  Try echo
+                </HoverBorderGradient>
+              </Link>
+            </div>
+            <h1 className="text-4xl md:text-5xl tracking-tighter pb-7 text-center max-w-3xl mx-auto">
+              Personal memory for AI, helping make conversations more effective.
             </h1>
-
           </div>
         }
       >
         <div className="flex items-center justify-center w-full h-full">
-          {/* Add your content here */}
+          <div className="relative w-[80vw] h-[60vh] border border-white rounded-2xl">
+            <Image
+              src="/productImage.png"
+              alt="Hero section image"
+              fill
+              className="object-contain rounded-2xl"
+              priority
+            />
+          </div>
         </div>
       </ContainerScroll>
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+
 export default function Home() {
   return (
     <div>
